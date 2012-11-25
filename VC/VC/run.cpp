3f241db64a7eabea8a6880f7433a8f0ac86a1b80
Mahdi Akkouh <akkouh.mahdi@gmail.com>
2012-11-25 09:08:24 +0100

    main program

--- VC/VC/run.cpp
+++ VC/VC/run.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 
+#include <gsl/gsl_rng.h>
+
 
 using namespace std;
 
+gsl_rng * r;  /* global generator */
 
 int main()
 {
-	return 0;
+    const gsl_rng_type * T;
+    gsl_rng * r;
+     
+    int i, n = 10;
+     
+    gsl_rng_env_setup();
+     
+    T = gsl_rng_default;
+    r = gsl_rng_alloc (T);
+     
+    for (i = 0; i < n; i++) 
+        {
+        double u = gsl_rng_uniform (r);
+        printf ("%.5f\n", u);
+        }
+     
+    gsl_rng_free (r);
+
+	char c;
+	cin >> c;
+     
+    return 0;
 }
